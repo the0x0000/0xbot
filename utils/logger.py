8ea8b03e390a53b4ed5fdc09679248ab8ade8166
halareka <149045655+halareka@users.noreply.github.com>
2026-02-28 23:59:23 +0700

    test: test .yml

--- utils/logger.py
+++ utils/logger.py
@@ -4,4 +4,5 @@ def send_pretty_log(emoji, text):
     try:
         bot.send_message(ADMIN_LOG_CHAT, f"{emoji} {text}", parse_mode='Markdown')
     except Exception as e:
-        print(f"[LOG ERROR] {e}")
+        print(f"[LOG ERROR] {e}") 
+        # test
