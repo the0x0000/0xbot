7a1ded3dce95012cb8e72387879915c609c8a1a8
lolydev <jijadevjj@gmail.com>
2026-03-01 21:17:12 +0700

    fix: changed for multiple admins

--- bot_instance.py
+++ bot_instance.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = [int(x.strip()) for x in os.getenv("ADMIN_ID", "").split(",") if x.strip().isdigit()]
 ADMIN_LOG_CHAT = int(os.getenv("ADMIN_LOG_CHAT"))
 CHAT_LINK = os.getenv("CHAT_LINK")
 DB_FILE = os.getenv("DB_FILE", "users.json")
